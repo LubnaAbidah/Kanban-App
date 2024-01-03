9b063ba755060ad20794ae9e0f14a5b69e22bfe7
Lubna Abidah <lubna16abidah@gmail.com>
2024-01-03 20:12:17 +0700

    Develop common layout

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,13 @@
+<div class="sidebar">
+  <div class="sidebar-container">
+  <a class="sidebar-link" href="{{ route('home') }}"> <!-- Diperbarui -->
+      <span class="material-icons sidebar-icon">home</span>
+      <p class="sidebar-text">Home</p>
+    </a>
+    <!-- Diperbarui -->
+    <a class="sidebar-link" href="{{ route('tasks.index') }}">
+      <span class="material-icons sidebar-icon">list</span>
+      <p class="sidebar-text">Task List</p>
+    </a>
+  </div>
+</div>
